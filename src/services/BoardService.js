6b81725307e471b5450cfec69312ce68c1f5c1a8
salted26 <arkim0630@gmail.com>
2025-03-05 10:56:28 +0900

    jpa select, insert, update 완료

--- src/services/BoardService.js
+++ src/services/BoardService.js
@@ -8,6 +8,7 @@ export const selectBoard = (no) => axios.get(`${REST_API_BASE_URL}/${no}`);
 
 export const createBoard = (board) => axios.post(`${REST_API_BASE_URL}/write`, board);
 
+// mybatis
 // export const updateBoard = (no, board) => axios.post(`${REST_API_BASE_URL}/update/${no}`, board);
 
 export const updateBoard = (no, board) => axios.put(`${REST_API_BASE_URL}/update/${no}`, board);
